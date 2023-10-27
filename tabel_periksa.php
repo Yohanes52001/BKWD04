16dc16b8661719e36cd73a68a2f4f6821a4d368f
Yohanes52001 <111202012551@mhs.dinus.ac.id>
2023-10-27 18:32:20 +0700

    poliklinik

--- tabel_periksa.php
+++ tabel_periksa.php
@@ -14,6 +14,22 @@
         ?>
             <option value="<?php echo $data['id'] ?>" <?php echo $selected ?>><?php echo $data['nama'] ?></option>
         <?php
+        $result = mysqli_query($mysqli, "SELECT pr.*,d.nama as 'nama_dokter', p.nama as 'nama_pasien' FROM periksa pr LEFT JOIN dokter d ON (pr.id_dokter=d.id) LEFT JOIN pasien p ON (pr.id_pasien=p.id) ORDER BY pr.tanggal_periksa DESC");
+        $no = 1;
+        while ($data = mysqli_fetch_array($result)) {
+            <tr>
+                <td><?php echo $no++ ?></td>
+                <td><?php echo $data['nama_pasien'] ?></td>
+                <td><?php echo $data['nama_dokter'] ?></td>
+                <td><?php echo $data['tanggal_periksa'] ?></td>
+                <td><?php echo $data['catatan'] ?></td>
+                <td>
+                    <a class="btn btn-success rounded-pill px-3"
+                    href="index.php?page=periksa&id=<?php echo $data['id'] ?>&aksi=hapus">Hapus</a>
+                </td>
+                </tr>
+        <?php
+        }
         ?>
     </select>
 </div>
