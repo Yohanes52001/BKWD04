205b996bc9681791c08f0b068d761c5fb67d6841
Yohanes52001 <111202012551@mhs.dinus.ac.id>
2023-10-27 09:37:57 +0700

    poliklinik

---
+++ form.php
@@ -0,0 +1,17 @@
+<?php
+$isi = '';
+$tgl_awal = '';
+$tgl_akhir = '';
+if (isset($_GET['id'])) {
+    $ambil = mysqli_query($mysqli, "SELECT * FROM kegiatan 
+    WHERE id='" . $_GET['id'] . "'");
+    while ($row = mysqli_fetch_array($ambil)) {
+        $isi = $row['isi'];
+        $tgl_awal = $row['tgl_awal'];
+        $tgl_akhir = $row['tgl_akhir'];
+    }
+?>
+    <input type="hidden" name="id" value="<?php echo $_GET['id'] ?>">
+<?php
+}
+?>
